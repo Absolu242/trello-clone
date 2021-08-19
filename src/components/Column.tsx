df693c37654ecdf8c61ae33d8cac7e7d5c62f8c4
Rahan <wangleibniz01@gmail.com>
2021-08-19 18:39:31 +0800

    created column component and defined default styles

---
+++ src/components/Column.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ColumnContainer, ColumnTitle } from '../styles'
+
+export default function Column() {
+   return (
+      <ColumnContainer>
+         <ColumnTitle>Column Title</ColumnTitle>
+      </ColumnContainer>
+   )
+}
