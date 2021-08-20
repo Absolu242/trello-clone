2333728506391639f5a3e0bbbc747d10157eed5e
Rahan <wangleibniz01@gmail.com>
2021-08-20 11:06:58 +0800

    successfuly implemented the adding list and task

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -7,10 +7,11 @@ import Card from "./Card"
 interface ColumnProps {
   text: string
   index: number
+  id: string
 }
 
-export default function Column({ text, index }: ColumnProps) {
-  const { state } = useAppState()
+export default function Column({ text, index, id }: ColumnProps) {
+  const { state, dispatch } = useAppState()
 
   return (
     <ColumnContainer>
@@ -20,7 +21,9 @@ export default function Column({ text, index }: ColumnProps) {
       ))}
       <AddNewItem
         toggleButtonText='+ Add another task'
-        onAdd={console.log}
+        onAdd={(text) =>
+          dispatch({ type: "ADD_TASK", payload: { text, taskId: id } })
+        }
         dark
       />
     </ColumnContainer>
--- src/store/AppStateContext.tsx
+++ src/store/AppStateContext.tsx
@@ -1,4 +1,6 @@
-import React, { createContext, useContext } from "react"
+import React, { createContext, useContext, useReducer } from "react"
+import { v4 as uuidv4 } from "uuid"
+import { findItemIndexById } from "../utils/findItemIndexById"
 
 interface Task {
   id: string
@@ -14,6 +16,10 @@ interface List {
 export interface AppState {
   lists: List[]
 }
+
+type Action =
+  | { type: "ADD_LIST"; payload: string }
+  | { type: "ADD_TASK"; payload: { text: string; taskId: string } }
 
 const appData: AppState = {
   lists: [
@@ -35,8 +41,40 @@ const appData: AppState = {
   ],
 }
 
+const appStateReducer = (state: AppState, action: Action): AppState => {
+  switch (action.type) {
+    case "ADD_LIST": {
+      return {
+        ...state,
+        lists: [
+          ...state.lists,
+          { id: uuidv4(), text: action.payload, tasks: [] },
+        ],
+      }
+    }
+    case "ADD_TASK": {
+      const targetLaneIndex = findItemIndexById(
+        state.lists,
+        action.payload.taskId
+      )
+      state.lists[targetLaneIndex].tasks.push({
+        id: uuidv4(),
+        text: action.payload.text,
+      })
+
+      return {
+        ...state,
+      }
+    }
+    default: {
+      return state
+    }
+  }
+}
+
 interface AppStateContextProps {
   state: AppState
+  dispatch: React.Dispatch<any>
 }
 
 const AppStateContext = createContext<AppStateContextProps>(
@@ -44,8 +82,10 @@ const AppStateContext = createContext<AppStateContextProps>(
 )
 
 export function AppStateProvider({ children }: React.PropsWithChildren<{}>) {
+  const [state, dispatch] = useReducer(appStateReducer, appData)
+
   return (
-    <AppStateContext.Provider value={{ state: appData }}>
+    <AppStateContext.Provider value={{ state, dispatch }}>
       {children}
     </AppStateContext.Provider>
   )
